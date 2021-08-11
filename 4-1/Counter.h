4a682130499a301669650e03236682f829af1fd9
leejunwoo0202 <gs04043@naver.com>
2021-08-11 19:21:39 +0900

    co

---
+++ 4-1/Counter.h
@@ -0,0 +1,22 @@
+#ifndef COUNTER_H_INCLUDED
+#define COUNTER_H_INCLUDED
+
+class Counter 
+{
+	int value;
+public:
+	void reset()
+	{
+		value = 0;
+	}
+	void count()
+	{
+		++value;
+	}
+	int getValue() const
+	{
+		return value;
+	}
+};
+
+#endif
